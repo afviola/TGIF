1462b0940bfe2a20abbdb51989d73c8bc5fef1c8
Federico Viola <twisterfav_93@hotmail.com>
2019-05-14 23:26:46 -0300

    agregado dibujaTablas.js

---
+++ scripts/dibujaTablas.js
@@ -0,0 +1,52 @@
+"use strict";
+
+function agregarDatosEnTabla(tabla, ...datosDeFila) {
+  let fila = tabla.insertRow(-1);
+
+  datosDeFila.forEach(function(dato) {
+    let celda = fila.insertCell(-1);
+    celda.innerHTML = dato;
+  });
+}
+
+function crearGlanceTable() {
+  let tabla = document.getElementById('at-glance-table');
+  agregarDatosEnTabla(tabla, "Democrats", estadisticas["number-of-democrats"], estadisticas["democrats-average-votes-with-party"]);
+  agregarDatosEnTabla(tabla, "Republicans", estadisticas["number-of-republicans"], estadisticas["republicans-average-votes-with-party"]);
+  agregarDatosEnTabla(tabla, "Independents", estadisticas["number-of-independents"], estadisticas["independents-average-votes-with-party"]);
+}
+
+function getNombreCompleto(miembro) {
+  return miembro.first_name + " " + (miembro.middle_name || "") + " " + miembro.last_name;
+}
+
+function crearTablasGenericas() {
+  let tablaLeast, tablaMost;
+
+  tablaLeast = document.getElementById('least-table');
+  tablaMost = document.getElementById('most-table');
+
+  //determino si estoy en un html de tipo attendance o loyalty
+  if (window.location.pathname.includes('attendance')) {
+    estadisticas["least-engaged"].forEach(function(miembro) {
+      agregarDatosEnTabla(tablaLeast, getNombreCompleto(miembro), miembro.missed_votes, miembro.missed_votes_pct);
+    });
+
+    estadisticas["most-engaged"].forEach(function(miembro) {
+      agregarDatosEnTabla(tablaMost, getNombreCompleto(miembro), miembro.missed_votes, miembro.missed_votes_pct);
+    });
+  } else {
+    estadisticas["least-loyal"].forEach(function(miembro) {
+      agregarDatosEnTabla(tablaLeast, getNombreCompleto(miembro), miembro.missed_votes, miembro.missed_votes_pct);
+    });
+  
+    estadisticas["most-loyal"].forEach(function(miembro) {
+      agregarDatosEnTabla(tablaMost, getNombreCompleto(miembro), miembro.missed_votes, miembro.missed_votes_pct);
+    });
+  }
+}
+
+crearGlanceTable();
+crearTablasGenericas();
+
+
--- scripts/estadisticas.js
+++ scripts/estadisticas.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const miembros = data.results[0].members;
+
 const estadisticas = {
   "number-of-democrats": 0,
   "number-of-republicans": 0,
@@ -14,17 +16,15 @@ const estadisticas = {
   "most-loyal": []
 };
 
-const miembrosTotales = {
-  miembros: null,
+const partidos = {
   republicanos: null,
   democratas: null,
   independientes: null,
 
   inicializarMiembros: function() {
-    this.miembros = data.results[0].members;
-    this.republicanos = this.miembros.filter(m => m.party === "R");
-    this.democratas = this.miembros.filter(m => m.party === "D");
-    this.independientes = this.miembros.filter(m => m.party === "I");
+    this.republicanos = miembros.filter(m => m.party === "R");
+    this.democratas = miembros.filter(m => m.party === "D");
+    this.independientes = miembros.filter(m => m.party === "I");
   }
 };
 
@@ -35,28 +35,26 @@ function votoPromedioConPartido(miembrosDeUnPartido) {
 }
 
 function get10PctMiembrosSegun(key, fnOrdenamiento) {
-  let listado, valorLimite;
-
-  listado = miembrosTotales.miembros;
-  listado.sort(fnOrdenamiento);
-  valorLimite = listado[Math.round(listado.length * 0.1) - 1][key];
+  miembros.sort(fnOrdenamiento);
   
-  if (listado[0][key] >= valorLimite) {
-    return listado.filter(m => m[key] >= valorLimite);
+  let valorLimite = miembros[Math.round(miembros.length * 0.1) - 1][key];
+  
+  if (miembros[0][key] >= valorLimite) {
+    return miembros.filter(m => m[key] >= valorLimite);
   } 
 
-  return listado.filter(m => m[key] <= valorLimite);
+  return miembros.filter(m => m[key] <= valorLimite);
 }
 
 function cargarEstadisticas() {
-  estadisticas["number-of-democrats"] = miembrosTotales.democratas.length;
-  estadisticas["number-of-independents"] = miembrosTotales.independientes.length;
-  estadisticas["number-of-republicans"] = miembrosTotales.republicanos.length;
-  estadisticas["total"] = miembrosTotales.miembros.length;
+  estadisticas["number-of-democrats"] = partidos.democratas.length;
+  estadisticas["number-of-independents"] = partidos.independientes.length;
+  estadisticas["number-of-republicans"] = partidos.republicanos.length;
+  estadisticas["total"] = miembros.length;
 
-  estadisticas["independents-average-votes-with-party"] = votoPromedioConPartido(miembrosTotales.independientes);
-  estadisticas["democrats-average-votes-with-party"] = votoPromedioConPartido(miembrosTotales.democratas);
-  estadisticas["republicans-average-votes-with-party"] = votoPromedioConPartido(miembrosTotales.republicanos);
+  estadisticas["independents-average-votes-with-party"] = votoPromedioConPartido(partidos.independientes);
+  estadisticas["democrats-average-votes-with-party"] = votoPromedioConPartido(partidos.democratas);
+  estadisticas["republicans-average-votes-with-party"] = votoPromedioConPartido(partidos.republicanos);
 
   estadisticas["most-engaged"] = get10PctMiembrosSegun("missed_votes_pct", (m1, m2) => m1.missed_votes_pct - m2.missed_votes_pct);
   estadisticas["least-engaged"] = get10PctMiembrosSegun("missed_votes_pct", (m1, m2) => m2.missed_votes_pct - m1.missed_votes_pct);
@@ -65,7 +63,7 @@ function cargarEstadisticas() {
   estadisticas["most-loyal"] = get10PctMiembrosSegun("missed_votes", (m1, m2) => m1.missed_votes - m2.missed_votes);
 }
 
-miembrosTotales.inicializarMiembros();
+partidos.inicializarMiembros();
 cargarEstadisticas();
 
 console.log(estadisticas);
